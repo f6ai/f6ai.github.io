7b29490452c030ccb3b4f2c34d1809cd09e983d5
Fanni Szikszai <fanniszikszai@gmail.com>
2019-08-27 16:06:18 +0200

    Updates

---
+++ precache-manifest.555b30ea1b640fda6f7cf49cd8246f50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a9fbbd920e78861bb14b2ff7392be4",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "dd2e0280b4bd75757b19",
+    "url": "/monsters/static/css/main.bd7cc86a.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monsters/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "dd2e0280b4bd75757b19",
+    "url": "/monsters/static/js/main.20160208.chunk.js"
+  },
+  {
+    "revision": "7cf1aa1e8095fd1ea510",
+    "url": "/monsters/static/js/runtime~main.bcd749e6.js"
+  }
+]);
